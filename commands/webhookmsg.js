3765f8a77edee7ea812684eb07f18cbe66dc13b2
papaj2139 <95711852+papaj2139@users.noreply.github.com>
2023-08-10 21:44:13 +0200

    DODAŁEM WYMAGANE UPRAWNIENIA
    do komend webhookmsg i wyczyść 
    wsumie nwm czy działają nie chce mi się testować

--- commands/webhookmsg.js
+++ commands/webhookmsg.js
@@ -3,9 +3,14 @@ const { WebhookClient, MessageEmbed } = require('discord.js');
 module.exports = {
   name: 'webhookmsg',
   description: 'Wysyła wiadomość przy użyciu webhooka.',
+  permissions: ['MANAGE_WEBHOOKS'], 
   async execute(message, args) {
     if (args.length < 2) {
       return message.reply('Użyj komendy w formacie: !webhookmsg <link webhooka> <wiadomość>');
+    }
+
+    if (!message.member.permissions.has(this.permissions)) {
+      return message.reply('Nie masz wystarczających uprawnień do użycia tej komendy.');
     }
 
     const webhookLink = args[0];
@@ -13,7 +18,7 @@ module.exports = {
 
     try {
       const webhookClient = new WebhookClient({ url: webhookLink });
-      await webhookClient.send(msgContent);
+      await webhookClient.send({ content: msgContent });
       message.reply('Wiadomość została wysłana przy użyciu webhooka.');
     } catch (error) {
       console.error('Błąd podczas wysyłania wiadomości:', error);
--- commands/wyczyść.js
+++ commands/wyczyść.js
@@ -1,21 +1,25 @@
 module.exports = {
-    name: 'wyczyść',
-    description: 'Usuwa określoną liczbę wiadomości.',
-    args: true,
-    usage: '<liczba>',
-    execute(message, args) {
-      const amount = parseInt(args[0]);
-  
-      if (isNaN(amount)) {
-        return message.reply('Podaj poprawną liczbę wiadomości do usunięcia.');
-      } else if (amount <= 0 || amount > 100) {
-        return message.reply('Podaj liczbę między 1 a 100.');
-      }
-  
-      message.channel.bulkDelete(amount, true).catch(error => {
-        console.error('Błąd podczas usuwania wiadomości:', error);
-        message.reply('Wystąpił błąd podczas usuwania wiadomości.');
-      });
-    },
-  };
-  
+  name: 'wyczyść',
+  description: 'Usuwa określoną liczbę wiadomości.',
+  permissions: ['MANAGE_MESSAGES'], 
+  args: true,
+  usage: '<liczba>',
+  execute(message, args) {
+    if (!message.member.permissions.has(this.permissions)) {
+      return message.reply('Nie masz wystarczających uprawnień do użycia tej komendy.');
+    }
+
+    const amount = parseInt(args[0]);
+
+    if (isNaN(amount)) {
+      return message.reply('Podaj poprawną liczbę wiadomości do usunięcia.');
+    } else if (amount <= 0 || amount > 100) {
+      return message.reply('Podaj liczbę między 1 a 100.');
+    }
+
+    message.channel.bulkDelete(amount, true).catch(error => {
+      console.error('Błąd podczas usuwania wiadomości:', error);
+      message.reply('Wystąpił błąd podczas usuwania wiadomości.');
+    });
+  },
+};
